3af78a73748b63b52ad4eaec836bc46f74eb6d64
Austin Graham <austingraham24@gmail.com>
2016-02-24 01:50:49 -0600

    fixed index

--- index.php
+++ index.php
@@ -55,8 +55,8 @@
         $result = $link->query("INSERT INTO users (firstName,lastName,email,phrase,admin) VALUES ('$fname', '$lname', '$email', '$password', 0)");
 
         $cookieValue = crypt($email,"itsrainingtacos");
-        setcookie("NewsAppAccess", $email, time()+180);  /* expire in 1 hour 3600*/
-        setcookie($email, $cookieValue, time()+180);  /* expire in 1 hour */
+        setcookie("NewsAppAccess", $fname.$lname, time()+180);  /* expire in 1 hour 3600*/
+        setcookie($fname.$lname, $cookieValue, time()+180);  /* expire in 1 hour */
         $loggedIn = true;
 
         if(!$result)
